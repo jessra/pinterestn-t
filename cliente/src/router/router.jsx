cc52a71995e0e1b74e2f8dff87b6c7680cf726f7
Aderauwu04 <aderabg1@gmail.com>
2023-02-21 09:42:59 -0400

    organización de archivos

--- cliente/src/router/router.jsx
+++ cliente/src/router/router.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { App } from "../App";
+import { App } from "../views/App";
 function Router() {
   return (
     <BrowserRouter>
