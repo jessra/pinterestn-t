0735dacb34c3bbde07362d3006e7e34de4b03d36
Aderauwu04 <aderabg1@gmail.com>
2023-02-22 10:59:10 -0400

    Creados estilos de scss en todas las vistas.

---
+++ cliente/src/components/ContenedorImg.jsx
@@ -0,0 +1,60 @@
+export default function ContenedorImg() {
+	return (
+    <div className="contenedor-galeria">
+      <div className="contenedor-img">
+        <img src="./src/imagenesTemporales/cyberpunk-edgerunners.jpg" alt="" />
+      </div>
+      <div className="contenedor-img">
+        <img src="./src/imagenesTemporales/photo_2023-01-27_11-16-59.jpg" alt="" />
+      </div>
+      <div className="contenedor-img">
+        <img src="./src/imagenesTemporales/Screenshot_20220622_190433.png" alt="" />
+      </div>
+      <div className="contenedor-img">
+        <img src="./src/imagenesTemporales/cyberpunk-edgerunners.jpg" alt="" />
+      </div>
+      <div className="contenedor-img">
+        <img src="./src/imagenesTemporales/photo_2023-01-27_11-16-59.jpg" alt="" />
+      </div>
+      <div className="contenedor-img">
+        <img src="./src/imagenesTemporales/Screenshot_20220622_190433.png" alt="" />
+      </div>
+      <div className="contenedor-img">
+        <img src="./src/imagenesTemporales/cyberpunk-edgerunners.jpg" alt="" />
+      </div>
+      <div className="contenedor-img">
+        <img src="./src/imagenesTemporales/photo_2023-01-27_11-16-59.jpg" alt="" />
+      </div>
+      <div className="contenedor-img">
+        <img src="./src/imagenesTemporales/Screenshot_20220622_190433.png" alt="" />
+      </div>
+      <div className="contenedor-img">
+        <img src="./src/imagenesTemporales/cyberpunk-edgerunners.jpg" alt="" />
+      </div>
+      <div className="contenedor-img">
+        <img src="./src/imagenesTemporales/photo_2023-01-27_11-16-59.jpg" alt="" />
+      </div>
+      <div className="contenedor-img">
+        <img src="./src/imagenesTemporales/Screenshot_20220622_190433.png" alt="" />
+      </div>
+      <div className="contenedor-img">
+        <img src="./src/imagenesTemporales/cyberpunk-edgerunners.jpg" alt="" />
+      </div>
+      <div className="contenedor-img">
+        <img src="./src/imagenesTemporales/photo_2023-01-27_11-16-59.jpg" alt="" />
+      </div>
+      <div className="contenedor-img">
+        <img src="./src/imagenesTemporales/Screenshot_20220622_190433.png" alt="" />
+      </div>
+      <div className="contenedor-img">
+        <img src="./src/imagenesTemporales/cyberpunk-edgerunners.jpg" alt="" />
+      </div>
+      <div className="contenedor-img">
+        <img src="./src/imagenesTemporales/photo_2023-01-27_11-16-59.jpg" alt="" />
+      </div>
+      <div className="contenedor-img">
+        <img src="./src/imagenesTemporales/Screenshot_20220622_190433.png" alt="" />
+      </div>
+    </div>
+	);
+}
